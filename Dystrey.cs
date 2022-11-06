8f8a0f52339b4f6060afdcfa6e1c325992b5cc89
tiwochka <117584470+tiwochka@users.noreply.github.com>
2022-11-06 18:33:45 +0300

    SIS
    kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkIk

---
+++ Dystrey.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dystrey : MonoBehaviour
+{
+    // 12424324bvxfdblhblblfbnsbndhqpsjcxllcvjdpojvpfdj
+    void Start()
+    {
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
